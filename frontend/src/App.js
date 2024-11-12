d85e6ea91041da2d1485b56bc4f0031bd64f817a
Brick Dalsis <brickdalsis@gmail.com>
2024-11-11 18:25:53 -0700

    changed styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,22 @@
 import React, { useState } from 'react';
 import KanbanBoard from './components/KanbanBoard';
 import NewJobForm from './components/NewJobForm';
-import './styles/App.css';
+import './App.css';
 
 const App = () => {
-    const [jobs, setJobs] = useState([]);
+  const [jobs, setJobs] = useState([]);
 
-    const handleJobCreated = (newJob) => {
-        setJobs((prevJobs) => [...prevJobs, newJob]);
-    };
+  const handleJobCreated = (newJob) => {
+    setJobs((prevJobs) => [...prevJobs, newJob]);
+  };
 
-    return (
-        <div className="app-container">
-            <h1>HVAC Job Tracker</h1>
-            <NewJobForm onJobCreated={handleJobCreated} />
-            <KanbanBoard jobs={jobs} setJobs={setJobs} />
-        </div>
-    );
+  return (
+    <div className="app-container">
+      <h1>HVAC Job Tracker</h1>
+      <NewJobForm onJobCreated={handleJobCreated} />
+      <KanbanBoard jobs={jobs} setJobs={setJobs} />
+    </div>
+  );
 };
 
 export default App;
