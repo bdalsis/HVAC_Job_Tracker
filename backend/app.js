70e86f6a98f17f69360754a27456c013aa228871
Brick Dalsis <brickdalsis@gmail.com>
2024-11-11 18:51:36 -0700

    backend now connected to database; form on frontend posts to database but does not display in table

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
+require('dotenv').config();  // This loads the variables from .env file
 
 const app = express();
 const port = 3001;
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT
+});
 
 app.use(cors());
 app.use(express.json());
